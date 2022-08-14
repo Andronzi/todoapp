7a19f9ed6aa2b7249bdc2137300bddd0e7d84bea
Andronzi <andrey.radioniv.2003@gmail.com>
2022-08-14 23:03:46 +0500

    some change in settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
       "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {},
+      fontSize: {
+        sm: ['14px', '20px'],
+      },
+
+      extend: {
+        colors: {
+          "black": "#000000",
+          "lightgrey": "#EAEAEA",
+          "darkgrey": "#5C5C5C",
+          "white": "#FFFFFF"
+        }
+      },
+
+      fontFamily: {
+        "nunito": ["Nunito"]
+      }
     },
     plugins: [],
   }
