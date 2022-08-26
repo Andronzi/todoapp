6750319419b362eb1f1929836b1dea9580cf52e1
Andronzi <andrey.radioniv.2003@gmail.com>
2022-08-26 22:57:37 +0500

    add styles for content title, subtitle

--- src/Components/Content/Content.tsx
+++ src/Components/Content/Content.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { RootState } from "@redux/store/store";
 import { useAppDispatch, useAppSelector } from "@redux/hooks";
 import { ContentState, setDate } from "@redux/store/contentSlice";
+import "./fonts.scss";
 
 const Content: React.FC = () => {
   const content: ContentState = useAppSelector(
@@ -46,10 +47,14 @@ const Content: React.FC = () => {
   }
 
   return (
-    <div className="bg-lightgrey w-screen h-screen ml-4">
-      <h1>{content.groupName}</h1>
+    <div className="content bg-lightgrey w-screen h-screen ml-4">
+      <h1 className="content-title font-nunito ml-16 mt-8">
+        {content.groupName}
+      </h1>
 
-      <p>{content.isHome ? content.date : ""}</p>
+      <p className="content-subtitle font-nunito ml-16 text-darkgrey">
+        {content.isHome ? content.date : ""}
+      </p>
     </div>
   );
 };
