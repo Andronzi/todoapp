40c781045f1ace175b73928f9956812ccf492d25
Andrey Radionov <andrey.radioniv.2003@gmail.com>
2022-09-04 10:14:59 +0700

    create post fetch

--- src/redux/store/contentSlice.ts
+++ src/redux/store/contentSlice.ts
@@ -9,6 +9,13 @@ type Task = {
   id: number;
 };
 
+type ClientTaskData = {
+  groupName: string;
+  value: string;
+  remindTime: Date;
+  completionTime: Date;
+};
+
 export const fetchTasksByGroupName = createAsyncThunk(
   "tasks/fetchByGroupNameStatus",
   async (groupName: string, thunkAPI) => {
@@ -20,6 +27,16 @@ export const fetchTasksByGroupName = createAsyncThunk(
     const result = data.filter((task: Task) => task.groupName === groupName);
 
     return result;
+  },
+);
+
+export const addTask = createAsyncThunk(
+  "tasks/addTask",
+  async (data: ClientTaskData, thunkAPI) => {
+    const response = await fetch(
+      "https://6307551fc0d0f2b8012cb7ad.mockapi.io/api/todo/tasks",
+      { method: "POST", body: JSON.stringify(data) },
+    );
   },
 );
 
@@ -67,6 +84,8 @@ export const contentSlice = createSlice({
       state.tasks = action.payload;
       state.isLoading = true;
     });
+
+    builder.addCase();
   },
 });
 
